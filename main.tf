7f77b77309b0b9738a769da44352996652181254
jirishus <jirishus@yahoo.com>
2021-10-15 20:27:05 -0700

    testing aws creds

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 ###################
+## Variables
+###################
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+###################
 ## Providers
 ###################
 
 provider "aws" {
-  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
 }
 
 ###################
@@ -30,6 +38,6 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Salamanders"
   }
 }
